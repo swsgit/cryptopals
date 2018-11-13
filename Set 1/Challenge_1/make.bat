1a598da148d41f13c63ed72f8717b53f73521843
swsgit <sws@tuta.io>
2018-11-13 00:38:28 -0800

    Rename Set 1/challenge_1/make.bat to Set 1/Challenge_1/make.bat

---
+++ Set 1/Challenge_1/make.bat
@@ -0,0 +1,1 @@
+clang hex2base64.c ../hex2str.c ../base64.c -I.. -O2 -Wall -Werror -Wextra -o h2b.exe
--- Set 1/challenge_1/make.bat
+++
@@ -1,1 +0,0 @@
-clang hex2base64.c ../hex2str.c ../base64.c -I.. -O2 -Wall -Werror -Wextra -o h2b.exe
