c92325ae29e0ba3e70f6ca9d89b7e6f77bc3d6ae
swsgit <sws@tuta.io>
2018-11-14 11:59:43 -0800

    reorganize

--- Set 1/binary.c
+++ Set 1/binary.c
@@ -24,16 +24,3 @@ long long d2b(int n) {
   }
   return binaryNumber;
 }
-
-size_t hamming_distance(const char *s1, const char *s2, size_t len) {
-  unsigned char val;
-  size_t dist = 0;
-  for (size_t i = 0; i < len; i++) {
-    val = s1[i] ^ s2[i];
-    while (val) {
-      ++dist;
-      val &= val - 1;
-    }
-  }
-  return dist;
-}
--- Set 1/binary.h
+++ Set 1/binary.h
@@ -1,3 +1,2 @@
 long long d2b(int n);
 int b2d(long long n);
-size_t hamming_distance(const char *s1, const char *s2, size_t len);
